283a13e5f5927510316de76774d0e04c8dd903b5
Philipp Wille <philipp.y.wille@gmail.com>
2019-12-19 22:28:50 +0100

    chore: rewrite sample parser to be more educational

--- src/parsers/sample.js
+++ src/parsers/sample.js
@@ -1,7 +1,12 @@
 module.exports = {
   name: 'sample',
   desc: 'is a sample parser.',
-  func: ({}) => (tokens, lines) => (
-    {err: [], jsons: tokens}
+  func: ({verbose}) => (tokens, lines) => (
+    // * Parse tokens to jsons
+    // * Collect error reports: {msg: String, line: Number, info: Token}
+    //   If verbose > 0, include line in error reports
+    //   If verbose > 1, include info in error reports
+    // * Return errors and parsed jsons
+    {err: [], jsons: []}
   )
 }
